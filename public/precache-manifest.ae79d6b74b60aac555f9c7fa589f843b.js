476f75b7d62ac37f31ab30a3f0867b972683de99
Galin Malchev <g.malchev@live.com>
2022-12-11 20:45:31 +0200

    first commit

---
+++ public/precache-manifest.ae79d6b74b60aac555f9c7fa589f843b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b195072dacc6ac426de300c3f10e0c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab09e7c3f2eb3bc79366",
+    "url": "/static/css/0.3df8cb10.chunk.css"
+  },
+  {
+    "revision": "f625df8b663838276091",
+    "url": "/static/css/4.475d84a6.chunk.css"
+  },
+  {
+    "revision": "195d10c3ea95d635d3d2",
+    "url": "/static/css/5.1326459c.chunk.css"
+  },
+  {
+    "revision": "acf039527c7b4296c896",
+    "url": "/static/css/6.295134cd.chunk.css"
+  },
+  {
+    "revision": "e12558401c837b6cc361",
+    "url": "/static/css/7.9a0d14f5.chunk.css"
+  },
+  {
+    "revision": "e171dd710cd36fb80add",
+    "url": "/static/css/8.c540964c.chunk.css"
+  },
+  {
+    "revision": "b69702dab6059df22dc0",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "ab09e7c3f2eb3bc79366",
+    "url": "/static/js/0.6e1379e0.chunk.js"
+  },
+  {
+    "revision": "53e11b38c3f18c105ac8",
+    "url": "/static/js/3.f3851e2e.chunk.js"
+  },
+  {
+    "revision": "f625df8b663838276091",
+    "url": "/static/js/4.979c7d4d.chunk.js"
+  },
+  {
+    "revision": "195d10c3ea95d635d3d2",
+    "url": "/static/js/5.fcf886ab.chunk.js"
+  },
+  {
+    "revision": "acf039527c7b4296c896",
+    "url": "/static/js/6.04863e76.chunk.js"
+  },
+  {
+    "revision": "e12558401c837b6cc361",
+    "url": "/static/js/7.4fc04a97.chunk.js"
+  },
+  {
+    "revision": "e171dd710cd36fb80add",
+    "url": "/static/js/8.34263555.chunk.js"
+  },
+  {
+    "revision": "b69702dab6059df22dc0",
+    "url": "/static/js/main.8767533e.chunk.js"
+  },
+  {
+    "revision": "e619d5e98880ff8a8b3b",
+    "url": "/static/js/runtime-main.c7fc41d9.js"
+  }
+]);
